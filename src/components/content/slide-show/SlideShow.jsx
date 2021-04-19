c687b322cabb9c827440d51869a749cd56a5ddb4
Ilya <dnamix1@gmail.com>
2021-04-19 14:45:40 +0300

    replacing ts for js

--- src/components/content/slide-show/SlideShow.jsx
+++ src/components/content/slide-show/SlideShow.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Indicators from './Indicators.tsx';
+import Indicators from './Indicators.jsx';
 import { CSSTransition } from 'react-transition-group';
 import './SlideShow.scss';
 
