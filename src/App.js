4bbe81ad306cbae50bdfd6eef9b67ec384a4dde0
Ilya Petrov <dnamix1@gmail.com>
2021-05-15 16:01:20 +0300

    started implementing details page

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import Header from './components/Header/Header.jsx';
 import './App.scss';
 import MainLayout from './components/MainLayout/MainLayout.jsx';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import MovieDetails from './components/content/details/MovieDetails.jsx';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Router>
-        <Route path="/" exact>
-          <MainLayout />
-        </Route>
+        <Switch>
+          <Route path="/" exact>
+            <MainLayout />
+          </Route>
+          <Route exact path="/movie/:id">
+            <MovieDetails />
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
